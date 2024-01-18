8f518ebdd1a4e65e4a06d3900d642fa1c0dec89d
dominic-source <chinonsodomnic@gmail.com>
2024-01-18 17:18:09 +0000

    select glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,7 +4,7 @@
 CREATE
 OR REPLACE 
 VIEW my_v3
-AS SELECT band_name, IF (split IS NOT NULL, split - formed, 2020 - formed) AS lifespan
+AS SELECT band_name, IF (split IS NOT NULL, split - formed, 2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

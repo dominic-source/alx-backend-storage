df4a6c5160e4a8625f62af0086623590d4a38607
dominic-source <chinonsodomnic@gmail.com>
2024-01-18 23:08:11 +0000

    index names

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,6 @@
 -- create index
 -- An index for a table creation
 
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names (name);
+UPDATE idx_name_first SET idx_name_first.name = LEFT(idx_name_first.name, 1);
 

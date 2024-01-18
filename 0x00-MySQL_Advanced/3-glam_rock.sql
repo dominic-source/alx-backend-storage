e5e4cbdcbcf8387580e8df9a0bf5b1eebcec0482
dominic-source <chinonsodomnic@gmail.com>
2024-01-18 17:16:00 +0000

    add order by

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,7 +6,7 @@ OR REPLACE
 VIEW my_v3
 AS SELECT band_name, IF (split IS NOT NULL, split - formed, 2020 - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
-
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
 SELECT * FROM my_v3;
 

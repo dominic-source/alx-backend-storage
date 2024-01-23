c67aa385165fe2648415a84358ebd3a934e68ec2
dominic-source <chinonsodomnic@gmail.com>
2024-01-23 18:10:58 +0100

    log stat

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""This module contains a function that update a document in a collection
+"""
+from pymongo import MongoClient
+import pprint
+
+
+def display_stats():
+    """update a document into a collection"""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    # Defining the conditions
+    conditions = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    values = []
+    # Create pipeline and their match condition
+    for condition in conditions:
+        pipeline = [
+            {
+                "$match": {"method": condition},
+                },
+            {
+                "$count": f"occurrences"
+            }]
+        result = list(collection.aggregate(pipeline))
+        data = result[0][f"occurrences"] if result else 0
+        values.append(data)
+
+    stat = collection.find({"path": "/status"}).count()
+
+    print("Methods:")
+    for idx, cond in enumerate(conditions):
+        print(f'\tmethod {cond}: {values[idx]}')
+
+    print(f"{stat} status check")
+
+
+if __name__ == '__main__':
+    display_stats()

2c707774e934e3043db19fdffc1923bba6ef93c8
dominic-source <chinonsodomnic@gmail.com>
2024-01-23 14:58:56 +0100

    list all document of a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
 """This module contains a function that list all document in a collection
 """
+from typing import List
 from pymongo import MongoClient
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection) -> List:
     """list all collection of a database"""
+
     if value := mongo_collection.find():
         return value
     return []
@@ -14,5 +15,5 @@ def list_all(mongo_collection):
 
 if __name__ == '__main__':
     client = MongoClient()
-    collection = client.my_db.school
-    list_all(collection)
+    mongo_collection = client.my_db.school
+    list_all(mongo_collection)

c67ce939ce30e0b563f8d1e7336b9fd09608e186
dominic-source <chinonsodomnic@gmail.com>
2024-01-23 15:57:25 +0100

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,8 +2,10 @@
 """This module contains a function that update a document in a collection
 """
 
+from typing import List
 
-def update_topics(mongo_collection, name, topics):
+
+def update_topics(mongo_collection, name: str, topics: List[str]) -> None:
     """update a document into a collection"""
-
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    if isinstance(name, str) and all([isinstance(item, str) for item in topics]):
+        mongo_collection.update({"name": name}, {"$set": {"topics": topics}})

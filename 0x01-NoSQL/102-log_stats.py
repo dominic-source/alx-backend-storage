8682a50311d4c9bb36ee99aeda08b351f4316985
dominic-source <chinonsodomnic@gmail.com>
2024-01-23 21:08:55 +0100

    advance log stat

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""This module contains a function that update a document in a collection
+"""
+from pymongo import MongoClient
+
+
+def display_stats():
+    """update a document into a collection"""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    # Defining the conditions
+    conditions = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    values = []
+    # Create pipeline and their match condition
+    for condition in conditions:
+        pipeline = [
+            {
+                "$match": {"method": condition},
+                },
+            {
+                "$count": f"occurrences"
+            }]
+        result = list(collection.aggregate(pipeline))
+        data = result[0][f"occurrences"] if result else 0
+        values.append(data)
+
+    # Create a pipeline for grouping ips
+    newdata = collection.aggregate([
+      {
+          "$group": {
+              "_id": "$ip",
+              "count_me": {"$sum": 1}
+              }
+      },
+      {
+          "$project": {
+              "_id": 0,
+              "ip": "$_id",
+              "count_me": 1
+          }
+      },
+      {
+          "$sort": {
+              "count_me": -1,
+          }
+      }
+    ])
+
+    stat = collection.find({"path": "/status"}).count()
+    print(f'{collection.count()} logs')
+    print("Methods:")
+    for idx, cond in enumerate(conditions):
+        print(f'\tmethod {cond}: {values[idx]}')
+
+    print(f"{stat} status check")
+
+    # display top 10 ips
+    print("IPs:")
+    newdata = list(newdata)
+    for dat in range(10):
+        print(f'\t{newdata[dat].get("ip")}: {newdata[dat].get("count_me")}')
+
+
+if __name__ == '__main__':
+    display_stats()

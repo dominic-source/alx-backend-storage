002ddb499149f38a8daace1c8db36d6642583af6
dominic-source <chinonsodomnic@gmail.com>
2024-01-24 10:03:08 +0100

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ from typing import List
 def update_topics(mongo_collection, name: str, topics: List[str]) -> None:
     """update a document into a collection"""
     if isinstance(name, str) and all([isinstance(item, str) for item in topics]):
-        mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
